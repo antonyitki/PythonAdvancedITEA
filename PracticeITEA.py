6e3a397730c3370b0ed62d8e8f1caee95ef4326c
Antony Itki <59201310+antonyitki@users.noreply.github.com>
2020-02-25 10:06:21 +0200

    Update PracticeITEA.py

--- PracticeITEA.py
+++ PracticeITEA.py
@@ -1,25 +1,2 @@
-for i in range (101):
-    if i%2 == 0:
-        print(i, end=' , ')
+#24/02/2020
 
-
-
-#slavar = {"Ua" : "Kiev", 'De' : "Berlin", "Ru" : "Moskva"}
-#strani = ["Ua", "Polska", "Ru"]
-#for i in strani:
-#    if i in slavar:
-#        print(slavar[i])
-
-
-Capitals = dict()
-Capitals['Russia'] = 'Moscow'
-Capitals['Ukraine'] = 'Kiev'
-Capitals['USA'] = 'Washington'
-Countries = ['Russia', 'France', 'USA', 'Russia']
-for country in Countries:
-    if country in Capitals:
-        print('The capital of ' + country + ' is ' + Capitals[country])
-
-
-
-if 
